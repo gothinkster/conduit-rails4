0bed336e8845dfd834198113649e360129159286
Albert Pai <albert@pai.email>
2016-03-29 21:10:13 -0700

    Add favorites to articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,25 @@ class User < ActiveRecord::Base
   validates :username, uniqueness: true, presence: true, allow_blank: false
 
   has_many :articles, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   def generate_jwt
     JWT.encode({ id: self.id,
                 exp: 60.days.from_now.to_i },
                Rails.application.secrets.secret_key_base)
+  end
+
+  def favorite(article)
+    favorites.find_or_create_by(article: article)
+  end
+
+  def unfavorite(article)
+    favorites.where(article: article).destroy_all
+
+    article.reload
+  end
+
+  def favorited?(article)
+    favorites.find_by(article_id: article.id).present?
   end
 end
